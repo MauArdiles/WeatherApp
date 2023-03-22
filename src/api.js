02757d9be0a19cbda6fc5c95ed09d34ff467febe
MeuReek <94654740+MeuReek@users.noreply.github.com>
2023-03-22 00:01:43 -0300

    Weather-App

---
+++ src/api.js
@@ -0,0 +1,16 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "96f1cdf9admsh3bc07ebc8ce1a9dp1a7bfcjsnad1ba8a3475f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+//Creamos constante para acceder a la URL de las ciudades
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+//Creamos constantes para acceder a la URL y Key del clima
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "100eb0d3099d2e490a4de2a80a8cfb26";
